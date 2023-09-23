0f8caf5fd8e1aa00559732fa6b1370edf3348cf7
Alex Terepin <aterepin@yandex.ru>
2023-09-23 17:53:31 +0500

    Install primeVue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Tailwind from "primevue/passthrough/tailwind";
 
-createApp(App).mount('#app')
+import '@/assets/style.css'
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';          
+
+const app = createApp(App);
+
+app.use(PrimeVue, { unstyled: true, pt: Tailwind });
+app.mount('#app');
