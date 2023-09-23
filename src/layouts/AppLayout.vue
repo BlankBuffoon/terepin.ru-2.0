6825a8a6af73f8da48d5d12763ca3cba65ab73e5
Alex Terepin <aterepin@yandex.ru>
2023-09-23 19:08:52 +0500

    Styles and layout

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -4,10 +4,16 @@
 
 <template>
     <Toast />
-    <h1>Hello World!</h1>
-    <div>
-        <router-view></router-view>
-    </div>
+    <main class="min-w-full">
+        <div class="grid grid-cols-2 gap-40 min-w-full">
+            <div>
+                <h1 class="font-bold text-10xl">Hello world!</h1>
+            </div>
+            <div>
+                <router-view></router-view>
+            </div>
+        </div>
+    </main>
 </template>
 
 <style scoped>
